da4aed55ce150d0d9d7f6ee99cd3036b234751d8
Wali <wali.rahman89@yahoo.com>
2019-10-02 22:18:24 -0400

    added start game screen

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+
 import Header from './components/Header'
+import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
+
   return (
 
     <View style={styles.screen}>
 
       <Header title="Guess a number"/>
+      <StartGameScreen/>
+
     </View>
   );
 }
